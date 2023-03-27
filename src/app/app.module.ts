6ac0a64cd6b4e4dd63b30c4c7cc35fd0496b6f63
ilyasBenismael <73490018+ilyasBenismael@users.noreply.github.com>
2023-03-27 11:18:15 +0000

    ilyas.v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaxeTrimCreateComponent } from './taxeTrim/taxe-trim-create/taxe-trim-create.component';
+import { NotificationLocaleCreateComponent } from './notificationLocale/notification-locale-create/notification-locale-create.component';
+import { TaxeAnnuelleCreateComponent } from './taxeAnnuelle/taxe-annuelle-create/taxe-annuelle-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaxeTrimCreateComponent,
+    NotificationLocaleCreateComponent,
+    TaxeAnnuelleCreateComponent
   ],
   imports: [
     BrowserModule,
