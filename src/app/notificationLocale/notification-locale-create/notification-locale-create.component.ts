7071b11e0ce6792337e2b95d6d15af4b5014f730
ilyasBenismael <73490018+ilyasBenismael@users.noreply.github.com>
2023-03-27 12:57:23 +0000

    ilyas.v1

---
+++ src/app/notificationLocale/notification-locale-create/notification-locale-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-locale-create',
+  templateUrl: './notification-locale-create.component.html',
+  styleUrls: ['./notification-locale-create.component.css']
+})
+export class NotificationLocaleCreateComponent {
+
+}
---
+++ src/app/taxeTrim/taxe-trim-create/taxe-trim-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taxe-trim-create',
+  templateUrl: './taxe-trim-create.component.html',
+  styleUrls: ['./taxe-trim-create.component.css']
+})
+export class TaxeTrimCreateComponent {
+
+}
